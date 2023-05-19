5d2c4743526b7cd163048909dc79903cc1fc5e7d
mty0327 <mun960327@gmail.com>
2023-05-19 10:50:37 +0900

    join

--- network/join.php
+++ network/join.php
@@ -1,27 +1,41 @@
-\<?php
+<?php
 // MySQL 데이터베이스 연결 설정
-$servername = "localhost"; // MySQL 서버 주소
-$nickname = "root"; // MySQL 사용자 닉네임
-$password = "cjtgmlakd12!"; // MySQL 사용자 비밀번호
-$dbname = "profile";
+$servername = "localhost";
+$username = "사용자_이름";
+$password = "비밀번호";
+$dbname = "데이터베이스_이름";
 
 // MySQL 연결 생성
-$conn = new mysqli($servername, $nickname, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // 연결 확인
 if ($conn->connect_error) {
-    die("MySQL 연결 실패: " . $conn->connect_error);
+    die("MySQL 연결 오류: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
+$sql = "CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nickname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)";
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . ", 이름: " . $row["name"];
-    }
+if ($conn->query($sql) === TRUE) {
+    echo "테이블이 생성되었습니다.";
 } else {
-    echo "결과가 없습니다";
+    echo "오류: " . $conn->error;
+}
+
+$nickname = $_POST['nickname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO users (nickname, email, password) VALUES ('$nickname', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "회원가입이 완료되었습니다.";
+} else {
+    echo "오류: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
